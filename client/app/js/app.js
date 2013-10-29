3c99f3788bbf65640a1a63e162d100043edba39f
James Walker <github@jzwalker.id.au>
2013-10-30 00:01:32 +1100

    Move admin partials into admin subdir

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -10,15 +10,15 @@ app.config(
      $routeProvider.when( '/admin',
                           {controller: 'AdminCtrl', templateUrl: 'partials/dashboard/admin.html'} );
      $routeProvider.when( '/projects',
-                          {controller: 'ProjectCtrl', templateUrl: 'partials/project/list.html'} );
+                          {controller: 'ProjectCtrl', templateUrl: 'partials/admin/project/list.html'} );
      $routeProvider.when( '/project/new',
-                          {controller: 'ProjectCtrl', templateUrl: 'partials/project/new.html'} );
+                          {controller: 'ProjectCtrl', templateUrl: 'partials/admin/project/new.html'} );
      $routeProvider.when( '/users',
-                          {controller: 'UserCtrl', templateUrl: 'partials/user/list.html'} );
+                          {controller: 'UserCtrl', templateUrl: 'partials/admin/user/list.html'} );
      $routeProvider.when( '/user/new',
-                          {controller: 'UserCtrl', templateUrl: 'partials/user/new.html'} );
+                          {controller: 'UserCtrl', templateUrl: 'partials/admin/user/new.html'} );
      $routeProvider.when( '/user/edit/:userId',
-                          {controller: 'UserEditCtrl', templateUrl: 'partials/user/edit.html'} );
+                          {controller: 'UserEditCtrl', templateUrl: 'partials/admin/user/edit.html'} );
      $routeProvider.when( '/todo',
                           {controller: 'TestCtrl', templateUrl: 'partials/dashboard/todo.html'} );
 
